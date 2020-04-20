5a7a093e8ea841ef88cc84d9e5ea16f864c4eb98
davisk83 <davisk83@uw.edu>
2020-04-20 13:52:30 -0700

    feedback implementation

--- app/src/main/java/com/davisk83/dotify/MainActivity.kt
+++ app/src/main/java/com/davisk83/dotify/MainActivity.kt
@@ -3,7 +3,6 @@ package com.davisk83.dotify
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.widget.TextView
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlin.random.Random
@@ -15,51 +14,50 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val playCount = findViewById<TextView>(R.id.play_count)
-        playCount.text = randomPlayCount.toString() + " plays"
+        play_count.text = getString(R.string.initialPlayCount).format(randomPlayCount)
 
-        play_button.setOnClickListener() {
-            randomPlayCount += 1
-            play_count.apply {
-                text = randomPlayCount.toString() + " plays"
-            }
-        }
+        initPrevClick()
+        initPlayClick()
+        initNextClick()
+        initChangeClick()
+        initApplyClick()
+    }
 
+    private fun initPrevClick() {
         previous_button.setOnClickListener() {
             Toast.makeText(this, "Skipping to previous track", Toast.LENGTH_SHORT).show()
         }
+    }
 
+    private fun initPlayClick() {
+        play_button.setOnClickListener() {
+            randomPlayCount += 1
+            play_count.text = getString(R.string.initialPlayCount).format(randomPlayCount)
+        }
+    }
+
+    private fun initNextClick() {
         next_button.setOnClickListener() {
             Toast.makeText(this, "Skipping to next track", Toast.LENGTH_SHORT).show()
         }
+    }
 
+    private fun initChangeClick() {
         change_user_button.setOnClickListener() {
-            username.apply {
-                visibility = View.INVISIBLE
-            }
-            new_username.apply {
-                visibility = View.VISIBLE
-            }
-            change_user_button.apply {
-                visibility = View.INVISIBLE
-            }
-            apply_button.apply {
-                visibility = View.VISIBLE
-            }
+            username.visibility = View.INVISIBLE
+            new_username.visibility = View.VISIBLE
+            change_user_button.visibility = View.INVISIBLE
+            apply_button.visibility = View.VISIBLE
         }
+    }
 
+    private fun initApplyClick() {
         apply_button.setOnClickListener() {
             val message = new_username.text.toString()
             if (message.trim().length > 0) {
-                apply_button.apply {
-                    visibility = View.INVISIBLE
-                }
-                change_user_button.apply {
-                    visibility = View.VISIBLE
-                }
-                new_username.apply {
-                    visibility = View.INVISIBLE
-                }
+                apply_button.visibility = View.INVISIBLE
+                change_user_button.visibility = View.VISIBLE
+                new_username.visibility = View.INVISIBLE
                 username.apply {
                     visibility = View.VISIBLE
                     text = new_username.text
